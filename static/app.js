177e832d71f22fd3ca67882e96a2f13b15e95776
garfield <otienot75@gmail.com>
2024-10-25 21:16:26 +0300

    base app with .gitignore modified

---
+++ static/app.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My Simple React App</h1>
+            <p>This is a basic React application.</p>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
